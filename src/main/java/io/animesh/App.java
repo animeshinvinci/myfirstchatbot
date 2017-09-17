d945dd985b0dc66f73aed638fc97dc79e7b3909c
animesh <animesh@ebctechnologies.com>
2017-09-17 23:19:20 +0530

    Initial commit

---
+++ src/main/java/io/animesh/App.java
@@ -0,0 +1,25 @@
+package io.animesh;
+
+import java.io.File;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.History;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.utils.IOUtils;
+
+
+/**
+ * Hello chatbot world!
+ *
+ */
+public class App 
+{
+    private static final boolean TRACE_MODE = false;
+    static String botName = "super";
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
